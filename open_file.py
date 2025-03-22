7fc6c3ab012cd58a4cc4800655e59314f5e03bed
Hilmi Özbay <ozbayyhilmi@gmail.com>
2025-03-22 05:27:29 +0300

    Dosya Açma İşlemleri

--- open_file.py
+++ open_file.py
@@ -1,5 +1,10 @@
 import cv2
 
 ## dosya okuma işlemi
+
+# imread -> dosya okuma işlemi yapar
+# imwrite -> dosya yazma işlemi yapar
+
+
 image = cv2.imread('image.png')
 cv2.imwrite("image_copy.jpeg", image)
